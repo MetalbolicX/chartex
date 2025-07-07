76a73298e08d466042361fd08f35dc562ac1b6bd
José Martínez Santana <jose.martinez.santana@gmail.com>
2025-07-06 20:45:22 -0600

    docs - Add the documentation files with docsify.

---
+++ docs/docs.js
@@ -0,0 +1,21 @@
+window.$docsify = {
+  name: "chartex",
+  repo: "https://github.com/MetalbolicX/chartex.git",
+  loadSidebar: true,
+  subMaxLevel: 3,
+  coverpage: true,
+  tabs: {
+    persist: true,
+    sync: true,
+    theme: "classic",
+    tabComments: true,
+    tabHeadings: true,
+  },
+  search: {
+    noData: "No results found",
+    paths: ["/api-reference"],
+    placeholder: "Search...",
+    depth: 2,
+    maxAge: 86400000, // 1 day
+  },
+};
