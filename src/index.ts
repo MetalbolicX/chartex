fb5dd4696d0ab23af6348da6eb9a62d988369bf5
José Martínez Santana <jose.martinez.santana@gmail.com>
2025-07-09 22:40:02 -0600

    feat - Add the exports for the library.

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import { bg, fg } from "./utils/utils.ts";
+import bar from "./charts/bar.ts";
+import bullet from "./charts/bullet.ts";
+import donut from "./charts/donut.ts";
+import gauge from "./charts/gauge.ts";
+import pie from "./charts/pie.ts";
+import scatter from "./charts/scatter.ts";
+
+import type {
+  BackgroundColor,
+  BarChartDatum,
+  BarChartOptions,
+  BulletChartDatum,
+  BulletChartOptions,
+  ChartDatum,
+  DonutChartDatum,
+  DonutChartOptions,
+  GaugeChartDatum,
+  GaugeChartOptions,
+  PieChartDatum,
+  PieChartOptions,
+  ScatterPlotDatum,
+  ScatterPlotOptions,
+} from "./types/types.ts";
+
+export {
+  bar,
+  bg,
+  bullet,
+  donut,
+  fg,
+  gauge,
+  pie,
+  scatter,
+  type BackgroundColor,
+  type BarChartDatum,
+  type BarChartOptions,
+  type BulletChartDatum,
+  type BulletChartOptions,
+  type ChartDatum,
+  type DonutChartDatum,
+  type DonutChartOptions,
+  type GaugeChartDatum,
+  type GaugeChartOptions,
+  type PieChartDatum,
+  type PieChartOptions,
+  type ScatterPlotDatum,
+  type ScatterPlotOptions,
+}
