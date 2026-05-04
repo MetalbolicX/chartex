eae2ab54821e632d23c604dd9dd0da2fcebfb4b0
José Martínez Santana <jose.martinez.santana@gmail.com>
2026-05-03 21:58:13 -0600

    chore(build): use concise re-exports in src/index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,29 +1,14 @@
 "use strict";
 
-import * as Bar from "./Charts/Bar.res.mjs";
-import * as Bullet from "./Charts/Bullet.res.mjs";
-import * as Pie from "./Charts/Pie.res.mjs";
-import * as Donut from "./Charts/Donut.res.mjs";
-import * as Gauge from "./Charts/Gauge.res.mjs";
-import * as Scatter from "./Charts/Scatter.res.mjs";
-import * as Sparkline from "./Charts/Sparkline.res.mjs";
-import * as Ansi from "./Core/Ansi.res.mjs";
-import * as Terminal from "./Core/Terminal.res.mjs";
-import * as Json from "./Core/Json.res.mjs";
-import * as Validate from "./Config/Validate.res.mjs";
-import * as Types from "./Config/Types.res.mjs";
-
-export {
-  Bar,
-  Bullet,
-  Pie,
-  Donut,
-  Gauge,
-  Scatter,
-  Sparkline,
-  Ansi,
-  Terminal,
-  Json,
-  Validate,
-  Types,
-};
+export * as Bar from "./Charts/Bar.res.mjs";
+export * as Bullet from "./Charts/Bullet.res.mjs";
+export * as Pie from "./Charts/Pie.res.mjs";
+export * as Donut from "./Charts/Donut.res.mjs";
+export * as Gauge from "./Charts/Gauge.res.mjs";
+export * as Scatter from "./Charts/Scatter.res.mjs";
+export * as Sparkline from "./Charts/Sparkline.res.mjs";
+export * as Ansi from "./Core/Ansi.res.mjs";
+export * as Terminal from "./Core/Terminal.res.mjs";
+export * as Json from "./Core/Json.res.mjs";
+export * as Validate from "./Config/Validate.res.mjs";
+export * as Types from "./Config/Types.res.mjs";
